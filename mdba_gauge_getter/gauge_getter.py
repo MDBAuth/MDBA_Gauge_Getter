5cabb2a638d9b26ef7aa46788230440e54c4f0fc
BenBMDBA <46876990+BenBMDBA@users.noreply.github.com>
2023-11-10 15:17:13 +1100

    Update gauge_getter.py
    always go to bom for nsw

--- mdba_gauge_getter/gauge_getter.py
+++ mdba_gauge_getter/gauge_getter.py
@@ -555,9 +555,9 @@ def gauge_pull(gauge_numbers: List[str], start_time_user: datetime.date, end_tim
 
     # log.info(f'Gauges by state is: {gauges_by_state}')
     data: List[List[List[Any]]] = []
-    nsw = process_gauge_pull(gauges_by_state['NSW'], 'NSW', 'CP', start_time_user,
-                               end_time_user, var, interval, data_type)
-    if not len(nsw) and len(gauges_by_state['NSW']) > 0:
+    #nsw = process_gauge_pull(gauges_by_state['NSW'], 'NSW', 'CP', start_time_user,
+    #                          end_time_user, var, interval, data_type)
+    if gauges_by_state['NSW'] #not len(nsw) and len(gauges_by_state['NSW']) > 0:
         log.warn(f'Data not available from NSW API, querying BOM...')
         gauges_by_state['BOM'] = gauges_by_state['NSW']
         nsw += gauge_pull_bom(gauges_by_state['BOM'], start_time_user, 
